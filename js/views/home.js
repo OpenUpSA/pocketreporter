7174ba8e5b3ea10e1cace3a1c0edaa515cf12825
Greg Kempe <greg@kempe.net>
2016-07-14 10:25:06 +0200

    Progress bar in story list

--- js/views/home.js
+++ js/views/home.js
@@ -26,10 +26,32 @@ var HomeView = Backbone.View.extend({
     var archived = StoryCheck.stories.where({archived: true}),
         current = StoryCheck.stories.where({archived: false});
 
+    function serialize(story) {
+      var d = story.toJSON();
+      d.percent_complete = story.percentComplete();
+      return d;
+    }
+
     this.$el.html(this.template({
       topics: StoryCheck.topics.toJSON(),
-      stories: _.map(current, function(s) { return s.toJSON(); }).reverse(),
-      archived: _.map(archived, function(s) { return s.toJSON(); }).reverse(),
+      stories: _.map(current, serialize).reverse(),
+      archived: _.map(archived, serialize).reverse(),
     }));
+
+    // progress bars
+    this.$('.story-list .percent').each(function() {
+      var p = $(this).data('value');
+      new ProgressBar.Circle(this, {
+        color: '#73c619',
+        trailColor: '#e6e6e6',
+        strokeWidth: 15,
+        text: {
+          value: Math.trunc(p * 100) + '%',
+          style: {
+            color: '#4a4a4a',
+          },
+        },
+      }).set(p);
+    });
   },
 });
