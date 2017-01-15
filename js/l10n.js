7d0e021bdd71b14a38d86f2648a5cdc6f96eddfc
Greg Kempe <greg@kempe.net>
2017-01-15 15:39:42 +0200

    en-za topics and questions

--- js/l10n.js
+++ js/l10n.js
@@ -9,10 +9,220 @@ var L10N = {
     },
     topics: {
       1: {
-        name: "News conference",
-        questions: {
+        "name": "News conference",
+        "questions": {
           1: "What were the major points made in the announcement of the press conference?",
-        },
+          2: "What are the names and job titles or official positions of speakers?",
+          3: "What is the background of major points of the conference?",
+          4: "What further info came out of the question and answer session?",
+          5: "What questions were not answered or avoided?",
+          6: "Tip: make sure you get copies of any handouts from the press conference",
+          7: "What are the contact details of the organisers (preferably phone numbers) in case of queries?"
+        }
+      },
+      2: {
+        "name": "Speech",
+        "questions": {
+          1: "What was the speaker's main point?",
+          2: "What is the name and title of the main speaker? (Make sure name is spelt correctly!)",
+          3: "What quotes do you have to support main point?",
+          4: "What was the purpose, time and location of the speech?",
+          5: "How many people attended and what was their makeup and reactions?",
+          6: "Did the speaker make any additional points?",
+          7: "What is the background to the major point they made?",
+          8: "What comments did the speaker's make before and after their speech?",
+          9: "Is there any additional info from the question and answer period",
+          10: "Do you have copies of speeches and/or background documents?",
+          11: "What are the contact details (preferably phone numbers/business cards) of speakers in case of queries?"
+        }
+      },
+      3: {
+        "name": "Protest March ",
+        "questions": {
+          1: "Tip: Ensure you have a large scarf and a bottle of vinegar (soak scarf in vinegar if police use teargas) in case there is teargas?",
+          2: "What kind of protest is it? A march, a strike, a barricade?",
+          3: "Who are the main organisers of the protest? Get their official title, organisation and contact details.",
+          4: "Was the protest planned or unplanned?",
+          5: "What is the reason for the protest?",
+          6: "Were any NGOs or political parties involved or related to the protest? Get their names, organisation, contact details and reason for involved.",
+          7: "What is the size of the crowd: don't guess! Check with organisers and police.",
+          8: "Get a quote from the organisers about the protest",
+          9: "Interview a minimum of 10 protesters: get their name, age, where they live, why they are protesting, what do they want to achieve?",
+          10: "Have there been any related protests in the past (how many, when and where) and was anything achieved?",
+          11: "How did the authorities and police respond to the protest?",
+          12: "Did police use any non-lethal force (teargas, rubber bullets) or lethal force (live ammunition), or both?",
+          13: "Was anyone injured? Who, how many and how badly?",
+          14: "To which hospitals/clinics were the injured taken for treatment?",
+          15: "If there are reports of violent force from police, do you have evidence and do you have a response from the police about this?",
+          16: "What is the current status of the protest?"
+        }
+      },
+      4: {
+        "name": "Motor Accidents",
+        "questions": {
+          1: "What time did the accident happen and where?",
+          2: "Were any people injured or dead and how many?",
+          3: "What type of vehicles were involved?",
+          4: "What is the identification of the drivers involved?  Names, ages, addresses, other personal information.",
+          5: "What was the cause of the accident? (according to paramedics, police or other official sources)",
+          6: "What is the identification of any others involved? (Passengers, pedestrians.)",
+          7: "What is the identification of the injured and dead?",
+          8: "What is the extent of any injuries?",
+          9: "What is the condition of the injured?",
+          10: "Where were the injured and dead taken? (Which hospital or morgue?)",
+          11: "Were there any acts of bravery or any rescues?",
+          12: "Were there any arrests or other legal action?",
+          13: "What is the damage to the vehicles?",
+          14: "What was the speed of vehicles involved?",
+          15: "Where were they coming from/going to?",
+          16: "Were there any strange weather or road conditions that could have caused the accident?",
+          17: "What happened according to eyewitnesses, investigating officers and people involved in incident?",
+          18: "What are the funeral arrangements for the dead?"
+        }
+      },
+      5: {
+        "name": "Health Services",
+        "questions": {
+          1: "What is the name of the hospital or clinic and where is it situated?",
+          2: "What services are not being offered when they should be? (areas of lack of service delivery)",
+          3: "Why are these services no longer being offered?",
+          4: "Who is affected by this? (A particular group of people / area and what are their socio-economic backgrounds?",
+          5: "How many people are affected, at risk or in danger because of the withdrawal of these services?",
+          6: "What are the names and ages of those affected and what are their experiences?",
+          7: "How long do patients wait to be seen by doctors/health workers?",
+          8: "Is there sufficient - and the correct - medication available?",
+          9: "Who is responsible for delivering the services?",
+          10: "What is the response from officials responsible for delivering the service?",
+          11: "Have patients visited the facility previously? If yes, was their file available when they were treated?",
+          12: "What distance did patients you interviewed have to travel to get there?",
+          13: "How did they get there? (bus, taxi, train, walk)",
+          14: "What was the cost of their return fare?"
+        }
+      },
+      6: {
+        "name": "Courts - Civil Action",
+        "questions": {
+          1: "Who is the person or organisation filing the action? Names, ages, addresses, other personal information.",
+          2: "What is the background of the plaintiff or petitioner: the person filing the action?",
+          3: "What is the background of the defendant or respondent?",
+          4: "What type of damage is alleged?",
+          5: "What is the outcome being sought?",
+          6: "What is the date of filing of the application and which is the court of jurisdiction?",
+          7: "Is there a special motivation behind the action (if any)?",
+          8: "What is the background to the conflict/disagreement?",
+          9: "Are there or have there been any similar cases decided by courts (precedents)?",
+          10: "Could this suit lead to a landmark action? Is it a precedent?",
+          11: "Is there a possibility of an out-of-court settlement?",
+          12: "What is the significance of the action and the effect on others?",
+          13: "What are the names of the lawyers for both sides? (Check spelling!)",
+          14: "What is the date and who is the presiding judge for the trial or hearing?"
+        }
+      },
+      7: {
+        "name": "Courts - Verdict",
+        "questions": {
+          1: "What is the verdict and damages, if awarded? (Same, less, greater than the damages sought?)",
+          2: "Who are the parties involved?",
+          3: "Did the judge  make a statement? Did it deviate from assessor's findings?",
+          4: "Give a summary of the allegations by plaintiff?",
+          5: "What was the key testimony and attorney/advocate's main points?",
+          6: "For how long did the court deliberate before judgement?",
+          7: "Are there any appeals or motions?"
+        }
+      },
+      8: {
+        "name": "Crime",
+        "questions": {
+          1: "What is the name and details of the victims? Names, ages, addresses, other personal information.",
+          2: "What was the crime and how was it committed?",
+          3: "What is the date, time and location of the crime? Confirm with official sources.",
+          4: "In the case of death, what was the official cause? (According to paramedics, police or other official sources.)",
+          5: "What injuries did the victims suffer and what weapon was used?",
+          6: "What was the motivation for the crime and what is the background of victim? (if relevant?)",
+          7: "Have there been any arrests and are there any suspects or clues?",
+          8: "Are there any strange circumstances surrounding the crime?",
+          9: "Get comment from the victim, witnesses, police and other official sources."
+        }
+      },
+      9: {
+        "name": "Fires ",
+        "questions": {
+          1: "How many people are dead and injured?",
+          2: "What is the identification of victims? Names, ages, addresses, other personal information.",
+          3: "Where was the fire and what was burning (a building, house, field, etc.)?",
+          4: "When did the fire start, where and how did it spread?",
+          5: "When and after how long was it brought under control?",
+          6: "How were victims killed and injured and what is the extent of the injuries?",
+          7: "Property loss: what is the extent, cost of damage, insurance? You may need to phone an expert.",
+          8: "Investigation: any unusual circumstances?",
+          9: "Where were the injured and dead taken? (Which hospital or morgue?)",
+          10: "Were there any acts of bravery or any rescues?",
+          11: "Get comments from neighbours, eyewitnesses.",
+          12: "How many fire engines and firefighters involved?",
+          13: "Sources: Fire chief, police, rescue services, hospital, mortuary, welfare agencies"
+        }
+      },
+      10: {
+        "name": "Sports ",
+        "questions": {
+          1: "What type of sport are you reporting on?",
+          2: "What is the name of the teams playing?",
+          3: "What was the final score?",
+          4: "Where and when did game take place?",
+          5: "What was the key play, strategy",
+          6: "Who were the outstanding players?",
+          7: "What effect does the result have on standings, rankings, individual records?",
+          8: "What was the scoring pattern of the game?",
+          9: "Are there any winning/losing streaks, records involved by team, player?",
+          10: "Get comments from coaches, players and fans of both teams.",
+          11: "What was the crowd size and how was their behaviour?",
+          12: "Were there any injuries?",
+          13: "How long was the game and did they run into extra time?",
+          14: "What are the game's statistics?"
+        }
+      },
+      11: {
+        "name": "Specific Illness",
+        "questions": {
+          1: "What is the name of the sickness/condition that the patient has?",
+          2: "What are the main symptoms?",
+          3: "What treatment did they receive?",
+          4: "Where are they are being treated?",
+          5: "Is the illness contagious and have there been any other reports of it locally or abroad?",
+          6: "What is the identification of victims? Names, ages, addresses, other personal information.",
+          7: "What comments, if any, did health workers make on the sickness?",
+          8: "What distance did patients you interviewed have to travel to get there and what is the cost of their return fares?"
+        }
+      },
+      12: {
+        "name": "Biography",
+        "questions": {
+          1: "What is the full name of the person you're writing about?",
+          2: "What is their age and date of birth?",
+          3: "What is their place of birth and where did they live?",
+          6: "What is their physical description? (This is important to add colour to your piece.)",
+          7: "Details of their character and personality? (Ask family and friends.)",
+          8: "Details of their education? (Include any degrees earned.)",
+          9: "Details of personal relationships with family, friends, work colleagues.",
+          10: "Details of awards / honours / formal recognition.",
+          12: "Summarise any of their major contributions to society and the world.",
+          13: "List at least THREE major political or social events which occurred during this person's life. For each event, explain how it may have affected your biographical subject."
+        }
+      },
+      13: {
+        "name": "Obituaries",
+        "questions": {
+          1: "What is the name, age, address, occupation of the deceased?",
+          2: "What was the time, place and cause of death?",
+          3: "What is the person's birthdate, birthplace?",
+          4: "Who are they survived by? (immediate family)",
+          5: "What are the funeral and burial arrangements?",
+          6: "What are the deceased's accomplishments?",
+          7: "What are the details of their education?",
+          8: "Were they members of any clubs or societies, such as a stokvel, Rotary or Lions?",
+          9: "Did they do military service? (formal or otherwise)",
+          10: "Anecdotes, recollections of friends, relatives?"
+        }
       }
     }
   },
